161273d402845ba145c2652d35397c61d2069bc4
Alejandro García <ale.garciamador@alum.uca.es>
2022-03-22 19:52:54 +0100

    Los niveles de la comunidad se sacan correctamente, en un array asociativo con la clave del id del nivel. Los niveles de la historia aún no se sacan, también esta preparada la función para subir niveles.

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -3,23 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Level;
+use App\Models\Score;
 use Illuminate\Http\Request;
 
 class LevelController extends Controller
 {
-    public function get_main_levels($user_id){
-        Level::where('user_id', 1)->get();
+    public function get_main_levels($user_id)
+    {
+        Level::select('id', 'name')->where('user_id', 1)->get();
     }
 
-    public function get_community_levels(){
-        $levels = Level::select('id', 'name');
+    public function get_community_levels()
+    {
+        $levels = Level::select('id', 'name')->where('user_id', '!=', 1)->get();
+        if (!$levels->isEmpty()) {
+            $result = [];
+            foreach ($levels as $l) {
+                $actual_level = [$l->id =>[ 'name' => $l->name]];
 
-    //     return json_encode(Level::join('scores', 'scores.level_id', '=', 'levels.id')->
-    //     select('levels.name', 'scores.value', 'scores.user_id')->orderBy('scores.value', 'desc')
-    //         ->where('user_id', '!=', 1)->take(3));
+                $top = Score::join('users', 'users.id', 'scores.user_id')->select('scores.value', 'users.name')->where('level_id', $l->id)->orderBy('value')->take(3)->get();
+                for ($i = 0; $i < $top->count(); $i++) {
+                    $actual_level[$l->id] += ["top_" . $i => ["score" => $top[$i]->value, "name" => $top[$i]->name]];
+                }
+                // array_push($result, $actual_level);
+                $result += $actual_level;
+            }
+        }
+        return json_encode($result);
     }
 
-    public function post_community_level(Request $request){
+    public function post_community_level(Request $request)
+    {
         $level = new Level();
         $level->user_id = $request->user_id;
         $level->file_name = $request->file_name;
@@ -32,9 +46,11 @@ class LevelController extends Controller
         $level->stairers = $request->stairers;
         $level->climbers = $request->climbers;
         $level->scene = $request->scene;
-        $level->save(); 
+        $level->save();
     }
-    public function get_level($id){
+
+    public function get_level($level_id)
+    {
         return false;
     }
 }
